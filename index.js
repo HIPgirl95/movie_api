e5336324af865748b90ac526f048e28496ed89d0
HIPgirl95 <hannah@dawne.com>
2024-07-29 14:52:22 -0400

    prepping movies

--- index.js
+++ index.js
@@ -2,14 +2,67 @@ const express = require("express"),
   morgan = require("morgan");
 const app = express();
 
+let Movies = [
+  {
+    title: "Pirates of the Caribbean",
+    actors: ["Johnny Depp", "Kiera Knightly", "Orlando Bloom"],
+    genre: ["Fantasy", "Comedy", "Adventure"],
+  },
+  {
+    title: "",
+    actors: [],
+    genre: [],
+  },
+  {
+    title: "",
+    actors: [],
+    genre: [],
+  },
+  {
+    title: "",
+    actors: [],
+    genre: [],
+  },
+  {
+    title: "",
+    actors: [],
+    genre: [],
+  },
+  {
+    title: "",
+    actors: [],
+    genre: [],
+  },
+  {
+    title: "",
+    actors: [],
+    genre: [],
+  },
+  {
+    title: "",
+    actors: [],
+    genre: [],
+  },
+  {
+    title: "",
+    actors: [],
+    genre: [],
+  },
+  {
+    title: "",
+    actors: [],
+    genre: [],
+  },
+];
+
 app.use(morgan("common"));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to my app!");
+app.get("/movies", (req, res) => {
+  res.json(Movies);
 });
 
-app.get("/secreturl", (req, res) => {
-  res.send("This is a secret URL with super top-secret content.");
+app.get("/", (req, res) => {
+  res.send("There is nothing here!.");
 });
 
 app.listen(8080, () => {

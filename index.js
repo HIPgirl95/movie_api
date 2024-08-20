6ef53927f4a62388d560a94efd5c85109621551d
HIPgirl95 <hannah@dawne.com>
2024-08-20 15:21:22 -0400

    GET user by Username

--- index.js
+++ index.js
@@ -115,13 +115,6 @@ let directors = [
   },
 ];
 
-// let users = [
-//   {
-//     username: "HannahHogan",
-//     password: "Pa55w0rd",
-//   },
-// ];
-
 app.use(morgan("common"));
 
 // GET a list of movies
@@ -187,6 +180,18 @@ app.get("/users", async (req, res) => {
   await Users.find()
     .then((users) => {
       res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
+//GET a user by username
+app.get("/users/:Username", async (req, res) => {
+  await Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
     })
     .catch((err) => {
       console.error(err);

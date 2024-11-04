3e522f486af93e93dad601f6384cd8b2dd18e320
HIPgirl95 <hannah@dawne.com>
2024-11-04 15:22:36 -0500

    Updating allowedOrigins

--- index.js
+++ index.js
@@ -30,24 +30,28 @@ mongoose.connect(process.env.CONNECTION_URI, {
 });
 
 app.use(morgan("common"));
-app.use(cors());
+// app.use(cors());
 
-// let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://testsite.com",
+  "https://my-flix-hannah-hogan.netlify.app/",
+];
 
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         let message =
-//           "The CORS policy for this application doesn't allow access from origin " +
-//           origin;
-//         return callback(new Error(message), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message =
+          "The CORS policy for this application doesn't allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 let auth = require("./auth")(app);
 
